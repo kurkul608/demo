b83dca40069cc138ac4f2b7c7a8ba2dfe41d0880
Petr Molchanov <molc.pyotr@yandex.ru>
2021-04-06 15:21:59 +0400

    add switch

--- front/src/ContentComponents/Content.jsx
+++ front/src/ContentComponents/Content.jsx
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 import HeaderComponent from "./HeaderComponent/HeaderComponent";
 import HomeComponent from "./MainComponents/HomeComponent/HomeComponent";
 import { Trending } from "./Trending/Trending";
@@ -15,8 +15,11 @@ export const Content = ({ videosDesctopList, channelLists }) => {
                 <div className="main-container">
                     {/* все страницы проекта */}
                     {/*<BrowserRouter>*/}
+                    <Switch>
                         <Route path='/home' render={() => <HomeComponent videosDesctopList={videosDesctopList} channelLists = {channelLists} />}  />
                         <Route path='/Trending' render={() => <Trending />} />
+                    </Switch>
+
                     {/*</BrowserRouter>*/}
                 </div>
             </div>
